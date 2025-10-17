4446fd5b96a6b27d6c4d7b8826da2723553d7789
aogden <aogden@ssnc-corp.global>
2025-10-17 12:56:42 +0100

    2022 day 2 complete

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,15 @@ mod y2022;
 fn main() {
     // Run all solutions
     // 2019
-    println!("Running: 2019 Day1 Part1");
     y2019::day1::part1();
-    println!("Running: 2019 Day1 Part2");
     y2019::day1::part2();
-    println!("Running: 2019 Day2 Part1");
     y2019::day2::part1();
-    println!("Running: 2019 Day2 Part2");
     y2019::day2::part2();
-    println!("Running: 2019 Day3 Part1");
     y2019::day3::part1();
 
     // 2022
-    println!("Running: 2022 Day1 Part1");
     y2022::day1::part1();
-    println!("Running: 2022 Day1 Part2");
     y2022::day1::part2();
+    y2022::day2::part1();
+    y2022::day2::part2();
 }
