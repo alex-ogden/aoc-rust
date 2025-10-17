8bdb1153d904d09edbf94b581c0fc60a2a97d6f5
aogden <aogden@ssnc-corp.global>
2025-10-17 11:40:14 +0100

    Adding 2022 day1 part 2

--- src/y2022/day1.rs
+++ src/y2022/day1.rs
@@ -5,7 +5,12 @@ pub fn part1() {
     let result = solve_part1(&input);
     println!("2022 :: Day 1 :: Part 1: {}", result);
 }
-pub fn part2() {}
+
+pub fn part2() {
+    let input: Vec<String> = utils::read_lines("inputs/2022/day1.txt");
+    let result = solve_part2(&input);
+    println!("2022 :: Day 1 :: Part 1: {}", result);
+}
 
 fn solve_part1(input: &[String]) -> u32 {
     input
@@ -18,23 +23,21 @@ fn solve_part1(input: &[String]) -> u32 {
         })
         .max()
         .unwrap_or(0)
-    /*
-        Less idiomatic way of doing it
-
-    let mut elf_calories: Vec<u32> = vec![0];
-    let mut current_elf: u32 = 0;
-
-    for line in input {
-        if line == "" {
-            current_elf += 1;
-            elf_calories.push(0);
-            continue;
-        }
-        elf_calories[current_elf as usize] += line.parse::<u32>().unwrap();
-    }
-
-    elf_calories.into_iter().max().unwrap()
-    */
 }
 
-fn solve_part2() {}
+fn solve_part2(input: &[String]) -> u32 {
+    let mut total_cals: Vec<u32> = input
+        .split(|line| line.is_empty())
+        .map(|group| {
+            group
+                .iter()
+                .filter_map(|line| line.parse::<u32>().ok())
+                .sum()
+        })
+        .collect();
+
+    total_cals.sort_unstable();
+    total_cals.reverse();
+
+    total_cals.iter().take(3).sum()
+}
