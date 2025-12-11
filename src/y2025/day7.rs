1558d040ce731d59d5b873aa3a1ace99e8f3ad30
aogden <aogden@ssnc-corp.global>
2025-12-11 10:14:13 +0000

    It works but it's slow as

--- src/y2025/day7.rs
+++ src/y2025/day7.rs
@@ -1,6 +1,6 @@
 use crate::utils;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub fn part1() {
     let input: Vec<Vec<String>> = utils::read_grid("inputs/2025/day7.txt");
@@ -63,45 +63,35 @@ fn solve_part2(input: &Vec<Vec<String>>) -> u64 {
     let width = input[0].len();
 
     let mut init_x_pos = 0;
+    let mut xpos_tracker: HashMap<u64, u64> = HashMap::new();
     for i in 0..width {
+        xpos_tracker.insert(i as u64, 0u64);
         if input[0][i] == "S" {
             init_x_pos = i;
         }
     }
 
-    let mut xpos_tracker = Vec::new();
-    xpos_tracker.push(init_x_pos);
-    let mut num_splits = 0;
+    *xpos_tracker.entry(init_x_pos as u64).or_default() += 1;
 
     for y in 0..height {
-        let mut to_remove = Vec::new();
-        let mut to_add = Vec::new();
-
-        for &xpos in xpos_tracker.iter() {
+        println!("Working on line {}/{}", y + 1, height);
+        for xpos in 0..width {
+            println!("\t{}/{} ", xpos + 1, width);
             if input[y][xpos] == "^" {
-                num_splits += 1;
-                to_remove.push(xpos);
-                to_add.push(xpos + 1);
-                to_add.push(xpos - 1);
+                for _ in 0..*xpos_tracker.get(&(xpos as u64)).unwrap() as u64 {
+                    *xpos_tracker.entry((xpos as u64) - 1 as u64).or_default() += 1;
+                    *xpos_tracker.entry((xpos as u64) + 1 as u64).or_default() += 1;
+                    xpos_tracker.insert(xpos as u64, 0);
+                }
             }
         }
-
-        for &xpos in to_remove.iter() {
-            xpos_tracker.remove(xpos);
-        }
-
-        for &xpos in to_add.iter() {
-            xpos_tracker.push(xpos);
-        }
-
-        println!("{:?}", xpos_tracker);
+        println!();
     }
 
-    let mut splits = HashSet::new();
-
-    for x_pos in xpos_tracker.iter() {
-        splits.insert(x_pos);
+    let mut total = 0;
+    for k in xpos_tracker.keys() {
+        total += xpos_tracker.get(k).unwrap();
     }
 
-    num_splits
+    total
 }
